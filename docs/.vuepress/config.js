edf2bbc3d41302e8473e326d8fa351839f909043
hongzhi-zach <zhhzh2217@hotmail.com>
2021-01-21 16:41:51 +0800

    first commit

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,47 @@
+module.exports = {
+    title: 'Phenix Guide',
+    themeConfig: {
+        smoothScroll: true,
+        nav: [
+            //{ text: 'Guide', link: '/'},
+            { text: 'Github', link: 'https://github.com/RobSenseTech/Phenix-Guide-Vuepress'},
+            { text: 'Robsense', link: 'http://www.robsense.com'}
+        ],
+        sidebarDepth: 0, // optional, defaults to 1
+        sidebar: [
+            {
+                title: 'Introducing Phenix Personal Edition',   
+                path: '/',      // optional, link of the title, which should be an absolute path and must exist
+                collapsable: true, // optional, defaults to true
+                //sidebarDepth: 1,    // optional, defaults to 1
+            },
+            {
+                title: 'Your First Flight With Phenix',   
+                path: '/chapter1/',     
+                collapsable: true,
+                children: [
+                  '/chapter1/build-your-quadcopter',
+                  '/chapter1/download-the-codebase'
+                ]
+            },
+            {
+                title: 'Robotics Intelligence and IoT',
+                path: '/chapter3-1/',
+                children: [
+                    { 
+                        title: 'Machine Vision', path: '/chapter3-1/machine-vision'
+                    },
+                    {
+                        title: 'Communication',   
+                        path: '/chapter3-1/communication/',
+                        children: [
+                            '/chapter3-1/communication/quick-connections'
+                        ]
+                    }
+                    
+                ],
+            }
+           
+        ]
+    }
+}
